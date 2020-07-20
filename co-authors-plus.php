b1a040387874d49248b149fadad31f2d7719feca
Mark Hughes <mark.hughes@leapdev.io>
2020-07-20 21:05:58 +1000

    Move licensing information

--- co-authors-plus.php
+++ co-authors-plus.php
@@ -5,25 +5,7 @@ Plugin URI: http://wordpress.org/extend/plugins/co-authors-plus/
 Description: Allows multiple authors to be assigned to a post. This plugin is an extended version of the Co-Authors plugin developed by Weston Ruter.
 Version: 4.0.0
 Author: refunktor, Mohammad Jangda, Daniel Bachhuber, Automattic
-Copyright: 2008-20202 Shared and distributed between Mark Hughes, Mohammad Jangda, Daniel Bachhuber, Weston Ruter
-
-GNU General Public License, Free Software Foundation <http://creativecommons.org/licenses/GPL/2.0/>
-This program is free software; you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation; either version 2 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program; if not, write to the Free Software
-Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
-
 -----------------
-
 Glossary:
 
 User - a WordPress user account
